b229bf13d1daa3f36f1597ec2ba8f1b53674c0c5
Alfonz <m.alfonz@gmail.com>
2015-06-14 02:53:11 +0800

    Fix Lastfm artist album

--- lib/DDG/Spice/Lastfm/ArtistAlbum.pm
+++ lib/DDG/Spice/Lastfm/ArtistAlbum.pm
@@ -15,25 +15,31 @@ category "entertainment";
 attribution github => ['https://github.com/jagtalon','Jag Talon'],
            twitter => ['http://twitter.com/juantalon','Jag Talon'];
 
+triggers startend => 'albums', 'records', 'cds';
+
 spice to => 'http://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.gettopalbums&artist=$1&autocorrect=1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
 
-my $synonyms = "albums?|records?|cds?";
-triggers query_lc => qr/^(?:$synonyms)\s+(?:(?:by|from|of)\s+)?([^\s]+(?:\s+[^\s]+)*)$
-                        |
-                        ^([^\s]+(?:\s+[^\s]+)*)\s+(?:$synonyms)$
-                        |
-                        ^([^\s]+(?:\s+[^\s]+)*)\s+(?:discography)$
-                        |
-                        ^(?:discography)\s+([^\s]+(?:\s+[^\s]+)*)$
-                        /x;
-
-
-
-handle matches => sub {
-    return $1 if $1;
-    return $2 if $2;
-    return $3 if $3;
-    return $4 if $4;
+handle remainder => sub {
+    return $_ if $_;
     return;
-};
-1;
+}
+
+# triggers query_lc => qr/^(?:$synonyms)\s+(?:(?:by|from|of)\s+)?([^\s]+(?:\s+[^\s]+)*)$
+#                         |
+#                         ^([^\s]+(?:\s+[^\s]+)*)\s+(?:$synonyms)$
+#                         |
+#                         ^([^\s]+(?:\s+[^\s]+)*)\s+(?:discography)$
+#                         |
+#                         ^(?:discography)\s+([^\s]+(?:\s+[^\s]+)*)$
+#                         /x;
+
+
+
+# handle matches => sub {
+#     return $1 if $1;
+#     return $2 if $2;
+#     return $3 if $3;
+#     return $4 if $4;
+#     return;
+# };
+# 1;
--- share/spice/lastfm/artist_album/lastfm_artist_album.js
+++ share/spice/lastfm/artist_album/lastfm_artist_album.js
@@ -1,36 +1,64 @@
-function ddg_spice_lastfm_artist_album (api_result) {
-    var skip = [
-        "albums",
-        "records",
-        "cd",
-        "cds"
-    ];
+(function (env){
+    "use strict";
 
-    // Don't do anything if we find an error.
-    if(api_result.error || !api_result.topalbums || !api_result.topalbums.album || api_result.topalbums.album.length === 0) {
-        return;
-    }
+    env.ddg_spice_lastfm_artist_album = function (api_result){
+        if(api_result.error){
+            return Spice.failed('lastfm_artist_album');
+        }
 
-    var artist = api_result.topalbums.album[0].artist.name;
-    if (DDG.isRelevant(artist, skip)) {
         Spice.add({
-            data              : api_result,
-            header1           : "Albums from " + artist,
-            sourceName       : "Last.fm",
-            sourceUrl        : "http://www.last.fm/search?q=" + artist + "&type=album",
-
-	    id        : "lastfm_artist_album",
-            template_frame    : "list",
-            templates  : {
-                items: api_result.topalbums.album,
-                item: Spice.lastfm_artist_album.lastfm_artist_album,
-                show: 3,
-                max: 10,
-                type: "ul"
+            id: 'lastfm_artist_album',
+            name: 'Albums',
+            data: api_result,
+            meta: {
+                sourceName: 'Last.fm',
+                sourceUrl        : "http://www.last.fm/search?q=" + api_result.topalbums['@attr'].artist + "&type=album",
             },
-
-            
-            
+            templates{
+                group: 'base',
+                options: {
+                    content: Spice.lastfm.artist_album.content
+                    moreAt: true
+                }
+            }
         });
     }
-};
+
+}(this));
+
+// function ddg_spice_lastfm_artist_album (api_result) {
+//     var skip = [
+//         "albums",
+//         "records",
+//         "cd",
+//         "cds"
+//     ];
+
+//     // Don't do anything if we find an error.
+//     if(api_result.error || !api_result.topalbums || !api_result.topalbums.album || api_result.topalbums.album.length === 0) {
+//         return;
+//     }
+
+//     var artist = api_result.topalbums.album[0].artist.name;
+//     if (DDG.isRelevant(artist, skip)) {
+//         Spice.add({
+//             data              : api_result,
+//             header1           : "Albums from " + artist,
+//             sourceName       : "Last.fm",
+//             sourceUrl        : "http://www.last.fm/search?q=" + artist + "&type=album",
+
+// 	    id        : "lastfm_artist_album",
+//             template_frame    : "list",
+//             templates  : {
+//                 items: api_result.topalbums.album,
+//                 item: Spice.lastfm_artist_album.lastfm_artist_album,
+//                 show: 3,
+//                 max: 10,
+//                 type: "ul"
+//             },
+
+            
+            
+//         });
+//     }
+// };
